e1bcff34459572323f45fbf3e0cfb89ca4792d21
AshfakYeafi <ashfakyeafifahim@gmail.com>
2021-08-24 22:56:35 +0600

    Delete main.c

--- books/main.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    float a,b;
-    printf("Enter Tow Numbers: ");
-    scanf("%f %f",&a,&b);
-    a>b?printf("%.2f is the bigest number",a):printf("%.2f is the bigest number",b);
-}
